2e8275108c966a004f6fe11085a5c395937aa8b6
zekel <zekelandes1214@gmail.com>
2021-11-18 11:11:09 -0500

    add css, sass, postcss, hotreloading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import "./styles/index.scss";
+
 const elvenShieldRecipe = {
 	leatherStrips: 2,
 	ironIngot: 1,
 	refinedMoonstone: 4
 }
 
+const elvenGauntletsRecipe = {
+	...elvenShieldRecipe,
+	leather: 1,
+	refinedMoonstone: 5
+}
+
 console.log(elvenShieldRecipe)
+console.log(elvenGauntletsRecipe)
