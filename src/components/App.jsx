e670b36cb79ff3d0e835e80014b9b864d909c419
zekel <zekelandes1214@gmail.com>
2021-11-29 13:05:21 -0500

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes"
+import hair from "../images/best_hair_day.png"
 
 const App = () => {
 	return (
@@ -9,9 +10,9 @@ const App = () => {
 				<section>
 					<h1>Oh Hai, React</h1>
 				</section>
+				<img src={hair} alt="hair" width="250" />
+				<Recipes />
 			</main>
-
-			<Recipes />
 		</>
 	);
 };
